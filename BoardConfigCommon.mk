58a8b69d85b7e71bcbf3d264db9590fb53cf84f0
Mark Petersen <mark@mtpetersen.com>
2015-10-25 10:14:22 +0100

    change for d2dcm
    Change-Id: Iab1763499e9313f40f43903df74976e93d580eec

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -27,7 +27,7 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 zcache androidbo
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01500000
 BOARD_KERNEL_PAGESIZE := 2048
-TARGET_KERNEL_SOURCE := kernel/samsung/d2
+TARGET_KERNEL_SOURCE := kernel/samsung/d2dcm
 
 # Enable dex-preoptimization to speed up first boot sequence
 ifeq ($(HOST_OS),linux)
